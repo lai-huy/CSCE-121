07b384f0117eba3421b84a29bdef095e35ab351e
lai-huy <98334390+lai-huy@users.noreply.github.com>
2022-04-15 20:54:48 -0500

    Simplify tester code

---
+++ [LW] Boolean Logic/test.cpp
@@ -0,0 +1,13 @@
+#include "boolean_functions.h"
+
+int main() {
+    booleanLogic1();
+    booleanLogic2();
+    booleanLogic3();
+    booleanLogic4();
+    booleanLogic5();
+    booleanLogic6();
+    booleanLogic7();
+
+    return 0;
+}
