e1d046dd62fdf2a5ebc16cf8ee866de8af5619af
lai-huy <98334390+lai-huy@users.noreply.github.com>
2022-04-15 21:11:07 -0500

    Update functions.h

--- [HW] Debugging, Input Validation, and Integer Overflow/functions.h
+++ [HW] Debugging, Input Validation, and Integer Overflow/functions.h
@@ -3,7 +3,7 @@
 
 /**
  * @brief return the largest of the arguments
- * 
+ *
  * @param a int 0
  * @param b int 1
  * @param c int 2
@@ -13,7 +13,7 @@ int Largest(int a, int b, int c);
 
 /**
  * @brief determine if the sum of the two arguments is even
- * 
+ *
  * @param a int 0
  * @param b int 1
  * @return true     if the sum of a and b is even
@@ -21,23 +21,57 @@ int Largest(int a, int b, int c);
  */
 bool SumIsEven(int a, int b);
 
-/// return the number of boxes needed to store the given number of apples
+/**
+ * @brief determins the number of boxes needed to store the given number of apples
+ *
+ * @param apples the number of apples
+ * @return int the number of boxes
+ */
 int BoxesNeeded(int apples);
 
-// return true if section A performs better than section B
-// throws std::invalid_argument if the arguments are invalid
+/**
+ * @brief determins if section A performs better than section b
+ *
+ * @param A_correct numer of correct answers in section A
+ * @param A_total   total number of answers in section A
+ * @param B_correct number of correct answers in section B
+ * @param B_total   total number of answers in section B
+ * @return true     if section A performs better than section B
+ * @return false    if section B performs better than section A
+ * @throws std::invalid_argument if the arguments are invalid
+ */
 bool SmarterSection(int A_correct, int A_total, int B_correct, int B_total);
 
 // return true if you had a good dinner
+/**
+ * @brief determine if you had a good dinner
+ *
+ * @param pizzas         the number of pizzas eaten
+ * @param is_weekend     if the pizzas were eaten on a weekend
+ * @return true          if the meal was a good dinner
+ * @return false         if the meal was not a good dinner
+ */
 bool GoodDinner(int pizzas, bool is_weekend);
 
-// return the sum of all integers from low to high (inclusive)
-// throw std::invalid_argument if low and high are out of order
-// throw std::overflow_error if the sum exceeds the maximum/minimum value of a signed 32-bit integer.
+/**
+ * @brief determine the sum of all integers from low to high (inclusive)
+ *
+ * @param low  lower bound
+ * @param high higher bound
+ * @return int sum of all integers from low to high (inclusive)
+ * @throw std::invalid_argument if low and high are out of order
+ * @throw std::overflow_error if the sum exceeds the maximum/minimum valyd of a signed 32-bit integer.
+ */
 int SumBetween(int low, int high);
 
-// return the product of a and b
-// throw std::overflow_error if the product exceeds the maximum/minimum value of a signed 32-bit integer.
+/**
+ * @brief calculate the prodict of a and b
+ *
+ * @param a    int a
+ * @param b    int b
+ * @return int a * b;
+ * @throws std::overflow_error if the product exceeds the maximum/minimum value of a signed 32-bit integer.
+ */
 int Product(int a, int b);
 
 #endif  // FUNCTIONS_H
